bc7628afd14fe2d47a0c18fcf407e90513080ecd
rofazhasan <139000822+rofazhasan@users.noreply.github.com>
2024-12-19 00:46:04 +0600

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,13 +1,16 @@
 
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-
+from dotenv import load_dotenv
+import os
 # Initialize the database
 db = SQLAlchemy()
 
 # Function to initialize the database with the Flask app
 def init_app(app):
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://rofaz-admin:TpuYgn8vfMK6@ep-delicate-hat-a1c6e7zy.ap-southeast-1.pg.koyeb.app/koyebdb'
+    load_dotenv()  # Load environment variables from .env file
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['SECRET_KEY'] = 'your-secret-key'
     db.init_app(app)
 
