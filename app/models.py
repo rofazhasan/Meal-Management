776ecee6ff53ffba896e4999a10b949d7e56357b
rofazhasan <139000822+rofazhasan@users.noreply.github.com>
2025-01-03 23:17:03 +0600

    Update models.py

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ class User(db.Model):
     __tablename__ = 'users'
     user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), nullable=True)
     phone = db.Column(db.String(15), nullable=False)
     password = db.Column(db.String(255), nullable=False)
     role = db.Column(db.Enum('user', 'admin', name='user_roles'), default='user', nullable=False)
